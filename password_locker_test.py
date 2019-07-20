e1aba3199c1e768a6d43ed6394c8b6fd95f299c0
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-07-20 15:37:22 +0300

    created three test methods for our Crentials class.i.e test_init where we created a new instance,save_account and save multiple accounts.we have to introduce tearDown method to enable us to save multiple accounts after an instance of creating a previous account

--- password_locker_test.py
+++ password_locker_test.py
@@ -20,46 +20,45 @@ class TestCredential(unittest.TestCase):
             "Linkedin", "EmmanuelMuchiri", "Emmanuel@7127")  # create contact object
 
     def test_init(self):
-        self.assertEqual(self.new_contact.first_name, "James")
-        self.assertEqual(self.new_contact.last_name, "Muriuki")
-        self.assertEqual(self.new_contact.phone_number, "0712345678")
-        self.assertEqual(self.new_contact.email, "james@ms.com")
+        self.assertEqual(self.new_credential.account_name, "James")
+        self.assertEqual(self.new_credential.user_name, "Muriuki")
+        self.assertEqual(self.new_credential.password, "0712345678")
 
-    def test_save_contact(self):
+    def save_account(self):
         '''
-        test_save_contact test case to test if the contact object is saved into
-         the contact list
+        test_save_account test case to test if the crential object is saved into
+         the credentials list
         '''
-        self.new_contact.save_contact()  # saving the new contact
-        self.assertEqual(len(Contact.contact_list), 1)
+        self.new_credential.save_account()  # saving the new credential
+        self.assertEqual(len(Credentials.credentials_list), 1)
 
     def tearDown(self):
             '''
             tearDown method that does clean up after each test case has run.
             '''
-            Contact.contact_list = []
+            Credentials.credentials_list = []
 
-    def test_save_multiple_contact(self):
+    def test_save_multiple_accounts(self):
             '''
-            test_save_multiple_contact to check if we can save multiple contact
-            objects to our contact_list
+            test_save_multiple_accounts to check if we can save multiple contact
+            objects to our Credentials_list
             '''
-            self.new_contact.save_contact()
-            test_contact = Contact(
-                "Test", "user", "0712345678", "test@user.com")  # new contact
-            test_contact.save_contact()
-            self.assertEqual(len(Contact.contact_list), 2)
+            self.new_credential.save_account()
+            test_credential = Credentials(
+            "Linkedin", "EmmanuelMuchiri", "Emmanuel@7127")  # new credential
+            test_credential.save_account()
+            self.assertEqual(len(Credentials.credentials_list), 2)
 
     def test_delete_contact(self):
             '''
             test_delete_contact to test if we can remove a contact from our contact list
             '''
-            self.new_contact.save_contact()
+            self.new_credential.save_contact()
             test_contact = Contact(
                 "Test", "user", "0712345678", "test@user.com")  # new contact
             test_contact.save_contact()
 
-            self.new_contact.delete_contact()  # Deleting a contact object
+            self.new_credential.delete_contact()  # Deleting a contact object
             self.assertEqual(len(Contact.contact_list), 1)
 
     def test_find_contact_by_number(self):
@@ -67,7 +66,7 @@ class TestCredential(unittest.TestCase):
         test to check if we can find a contact by phone number and display information
         '''
 
-        self.new_contact.save_contact()
+        self.new_credential.save_contact()
         test_contact = Contact("Test", "user", "0711223344",
                                "test@user.com")  # new contact
         test_contact.save_contact()
@@ -81,7 +80,7 @@ class TestCredential(unittest.TestCase):
         test to check if we can return a Boolean  if we cannot find the contact.
         '''
 
-        self.new_contact.save_contact()
+        self.new_credential.save_contact()
         test_contact = Contact("Test", "user", "0711223344",
                                "test@user.com")  # new contact
         test_contact.save_contact()
@@ -98,18 +97,18 @@ class TestCredential(unittest.TestCase):
         Test to confirm that we are copying the email address from a found contact
         '''
 
-        self.new_contact.save_contact()
+        self.new_credential.save_contact()
         Contact.copy_email("0712345678")
 
-        self.assertEqual(self.new_contact.email, pyperclip.paste())
+        self.assertEqual(self.new_credential.email, pyperclip.paste())
 
     def test_generate_Password(self):
-        self.new_contact.save_contact()
+        self.new_credential.save_contact()
         test_contact = Contact(
             "Test", "user", "0712345678", "test@user.com")  # new contact
         test_contact.save_contact()
 
-        self.new_contact.generate_Password()  # Deleting a contact object
+        self.new_credential.generate_Password()  # Deleting a contact object
         self.assertEqual(len(Contact.contact_list), 1)
 
 
@@ -131,21 +130,21 @@ class TestContact(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_contact = Contact(
+        self.new_credential = Contact(
             "James", "Muriuki", "0712345678", "qGRn2Y7@£")  # create contact object
 
     def test_init(self):
-        self.assertEqual(self.new_contact.first_name, "James")
-        self.assertEqual(self.new_contact.last_name, "Muriuki")
-        self.assertEqual(self.new_contact.phone_number, "0712345678")
-        self.assertEqual(self.new_contact.email, "qGRn2Y7@£")
+        self.assertEqual(self.new_credential.first_name, "James")
+        self.assertEqual(self.new_credential.last_name, "Muriuki")
+        self.assertEqual(self.new_credential.phone_number, "0712345678")
+        self.assertEqual(self.new_credential.email, "qGRn2Y7@£")
 
     def test_save_contact(self):
         '''
         test_save_contact test case to test if the contact object is saved into
          the contact list
         '''
-        self.new_contact.save_contact()  # saving the new contact
+        self.new_credential.save_contact()  # saving the new contact
         self.assertEqual(len(Contact.contact_list), 1)
 
     def tearDown(self):
@@ -159,7 +158,7 @@ class TestContact(unittest.TestCase):
             test_save_multiple_contact to check if we can save multiple contact
             objects to our contact_list
             '''
-            self.new_contact.save_contact()
+            self.new_credential.save_contact()
             test_contact = Contact(
                 "Test", "user", "0712345678", "test@user.com")  # new contact
             test_contact.save_contact()
@@ -169,12 +168,12 @@ class TestContact(unittest.TestCase):
             '''
             test_delete_contact to test if we can remove a contact from our contact list
             '''
-            self.new_contact.save_contact()
+            self.new_credential.save_contact()
             test_contact = Contact(
                 "Test", "user", "0712345678", "test@user.com")  # new contact
             test_contact.save_contact()
 
-            self.new_contact.delete_contact()  # Deleting a contact object
+            self.new_credential.delete_contact()  # Deleting a contact object
             self.assertEqual(len(Contact.contact_list), 1)
 
     def test_find_contact_by_number(self):
@@ -182,7 +181,7 @@ class TestContact(unittest.TestCase):
         test to check if we can find a contact by phone number and display information
         '''
 
-        self.new_contact.save_contact()
+        self.new_credential.save_contact()
         test_contact = Contact("Test", "user", "0711223344",
                                "test@user.com")  # new contact
         test_contact.save_contact()
@@ -196,7 +195,7 @@ class TestContact(unittest.TestCase):
         test to check if we can return a Boolean  if we cannot find the contact.
         '''
 
-        self.new_contact.save_contact()
+        self.new_credential.save_contact()
         test_contact = Contact("Test", "user", "0711223344",
                                "test@user.com")  # new contact
         test_contact.save_contact()
@@ -213,18 +212,18 @@ class TestContact(unittest.TestCase):
         Test to confirm that we are copying the email address from a found contact
         '''
 
-        self.new_contact.save_contact()
+        self.new_credential.save_contact()
         Contact.copy_email("0712345678")
 
-        self.assertEqual(self.new_contact.email, pyperclip.paste())
+        self.assertEqual(self.new_credential.email, pyperclip.paste())
 
     def test_generate_Password(self):
-        self.new_contact.save_contact()
+        self.new_credential.save_contact()
         test_contact = Contact(
             "Test", "user", "0712345678", "test@user.com")  # new contact
         test_contact.save_contact()
 
-        self.new_contact.generate_Password()  # Deleting a contact object
+        self.new_credential.generate_Password()  # Deleting a contact object
         self.assertEqual(len(Contact.contact_list), 2)
 
 
